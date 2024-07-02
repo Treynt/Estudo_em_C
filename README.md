574c19b3ee218c3981353f4e57191131a2d98fc8
Silvio Gonçalves <silvio.goncalves@aluno.uece.br>
2024-07-02 17:55:19 -0300

    Estudo em C

---
+++ README.md
@@ -0,0 +1,3 @@
+#Estou utilizando aqui apenas como repositório
+#Não estou rodando diretamente no VS Code
+#Para isso, utilizo o CodeBlocks para executar os testes
---
+++ intro.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    int a = 10;
+    int b, c;
+
+    printf("&a = %p, a = %d\n", &a, a);
+    printf("&b = %p, b = %d\n", &b, b);
+    printf("&c = %p, c = %d\n\n", &c, c);
+// &a faz a indicação que quer printar o endereço de memória da variável 'a'
+// No &a, temos que indicar o tipo de dado, e o endereço de memória é representado por %p
+// No 'a', como é inteiro, o tipo é %d, de digito
+// \n pular a linha, que nem python
+
+    b = 20;
+    c = b + c;
+
+    printf("&a = %p, a = %d\n", &a, a);
+    printf("&b = %p, b = %d\n", &b, b);
+    printf("&c = %p, c = %d\n\n", &c, c);
+
+
+    return 0;
+}
